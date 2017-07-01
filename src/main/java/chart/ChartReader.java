a69eb0d33014d9d118d58e41af646d3f2ff1158b
Glenn Sheasby <gsheasby@palantir.com>
2017-07-01 10:03:16 +0100

    Find a file with hard-coded date

--- src/main/java/chart/ChartReader.java
+++ src/main/java/chart/ChartReader.java
@@ -1,10 +1,19 @@
 package chart;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ChartReader {
     private final int week;
 
     public ChartReader(int week) {
         this.week = week;
         System.out.println("It's week " + week + "!");
+    }
+
+    public boolean findChart() {
+        Path path = Paths.get("src/main/resources", week + "-20170625.csv");
+        return Files.exists(path);
     }
 }
--- src/test/java/chart/ChartReaderTest.java
+++ src/test/java/chart/ChartReaderTest.java
@@ -1,10 +1,32 @@
 package chart;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChartReaderTest {
+    private ChartReader reader;
+
+    @Before
+    public void setUp() {
+        reader = new ChartReader(589);
+    }
+
     @Test
     public void create() {
-        ChartReader reader = new ChartReader(589);
+    }
+
+    // TODO can't find chart that isn't there
+    @Test
+    public void canFindChart() {
+        assertTrue(reader.findChart());
+    }
+
+    @Test
+    public void cannotFindNonExistentChart() {
+        ChartReader other = new ChartReader(1337);
+        assertFalse(other.findChart());
     }
 }
