461aa1d5327a58d4cdc19ee08a5a8252eee200cd
Glenn Sheasby <gsheasby@palantir.com>
2017-07-01 13:24:52 +0100

    First week: new entries

--- src/main/java/chart/ChartCompiler.java
+++ src/main/java/chart/ChartCompiler.java
@@ -1,6 +1,8 @@
 package chart;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChartCompiler {
     private final ChartReader reader;
@@ -9,7 +11,22 @@ public class ChartCompiler {
         this.reader = reader;
     }
 
-    public void compileChart(int week) throws IOException {
+    public Chart compileChart(int week) throws IOException {
         SimpleChart thisWeek = reader.findChart(week);
+
+        List<ChartEntry> entries = thisWeek.entries().stream()
+                                           .map(this::newEntry)
+                                           .collect(Collectors.toList());
+
+        return ImmutableChart.builder().entries(entries).build();
+    }
+
+    private ChartEntry newEntry(SimpleChartEntry simpleEntry) {
+        return ImmutableChartEntry.builder()
+                .position(simpleEntry.position())
+                .weeksOnChart(1)
+                .title(simpleEntry.title())
+                .artist(simpleEntry.artist())
+                .build();
     }
 }
--- src/test/java/chart/ChartCompilerTest.java
+++ src/test/java/chart/ChartCompilerTest.java
@@ -1,5 +1,7 @@
 package chart;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.junit.Test;
@@ -10,5 +12,22 @@ public class ChartCompilerTest {
         ChartReader reader = new ChartReader("src/test/resources");
         ChartCompiler compiler = new ChartCompiler(reader);
         compiler.compileChart(1);
+    }
+
+    // TODO add mocking
+    @Test
+    public void firstWeekHasNewEntries() throws IOException {
+        ChartReader reader = new ChartReader("src/test/resources");
+        ChartCompiler compiler = new ChartCompiler(reader);
+        Chart chart = compiler.compileChart(1);
+        assertEquals(1, chart.entries().size());
+
+        ChartEntry expected = ImmutableChartEntry.builder()
+                .position(1)
+                .title("title")
+                .artist("artist")
+                .weeksOnChart(1)
+                .build();
+        assertEquals(expected, chart.entries().get(0));
     }
 }
