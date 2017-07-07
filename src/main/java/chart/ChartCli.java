92e901dc4f335ef5b31b41779c787e5cbbc13bca
Glenn Sheasby <gsheasby@palantir.com>
2017-07-07 17:24:45 +0100

    Cli update

--- src/main/java/chart/ChartCli.java
+++ src/main/java/chart/ChartCli.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 
 public class ChartCli {
     private static final String FOLDER = "src/main/resources";
+    private static final String DERIVED_FOLDER = FOLDER + "/derived";
 
     public static void main(String[] args) throws IOException {
         int week = Integer.parseInt(args[0]);
         ChartReader reader = new ChartReader(FOLDER);
-        ChartCompiler compiler = new ChartCompiler(reader);
+        ChartReader derivedReader = new ChartReader(DERIVED_FOLDER);
+        ChartCompiler compiler = new ChartCompiler(reader, derivedReader);
         Chart chart = compiler.compileChart(week);
+
+        new ChartSaver(DERIVED_FOLDER).saveChart(chart);
 
         for (ChartEntry entry : chart.entries()) {
             printEntry(entry);
