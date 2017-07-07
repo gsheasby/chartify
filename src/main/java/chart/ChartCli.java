78fdf557d099f2699d72920e603ab6a93022b3c2
Glenn Sheasby <gsheasby@palantir.com>
2017-07-07 16:13:50 +0100

    Print chart

--- src/main/java/chart/ChartCli.java
+++ src/main/java/chart/ChartCli.java
@@ -9,6 +9,27 @@ public class ChartCli {
         int week = Integer.parseInt(args[0]);
         ChartReader reader = new ChartReader(FOLDER);
         ChartCompiler compiler = new ChartCompiler(reader);
-        compiler.compileChart(week);
+        Chart chart = compiler.compileChart(week);
+
+        for (ChartEntry entry : chart.entries()) {
+            printEntry(entry);
+        }
+    }
+
+    private static void printEntry(ChartEntry entry) {
+        String s;
+        if (entry.lastPosition().isPresent()) {
+            s = String.format("%02d (%02d) %s - %s",
+                              entry.position(),
+                              entry.lastPosition().get(),
+                              entry.title(),
+                              entry.artist());
+        } else {
+            s = String.format("%02d (NE) %s - %s",
+                              entry.position(),
+                              entry.title(),
+                              entry.artist());
+        }
+        System.out.println(s);
     }
 }
