1c73952cd2d543e4e0b5d28273fab6770dd89652
Glenn Sheasby <gsheasby@palantir.com>
2017-07-24 13:17:00 +0100

    Better error messaging

--- src/main/java/chart/CsvLineParser.java
+++ src/main/java/chart/CsvLineParser.java
@@ -7,16 +7,21 @@ import org.jetbrains.annotations.NotNull;
 public class CsvLineParser {
     public static SimpleChartEntry parse(String line) {
         int numElements = 3;
-        String[] split = getStrings(line, numElements);
 
-        int pos = Integer.parseInt(split[0]);
-        String title = split[1];
-        String artist = split[2];
-        return ImmutableSimpleChartEntry.builder()
-                                        .position(pos)
-                                        .title(title)
-                                        .artist(artist)
-                                        .build();
+        try {
+            String[] split = getStrings(line, numElements);
+
+            int pos = Integer.parseInt(split[0]);
+            String title = split[1];
+            String artist = split[2];
+            return ImmutableSimpleChartEntry.builder()
+                                            .position(pos)
+                                            .title(title)
+                                            .artist(artist)
+                                            .build();
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new RuntimeException("Failed to parse line: " + line, e);
+        }
     }
 
     public static ChartEntry parseEntry(String line) {
