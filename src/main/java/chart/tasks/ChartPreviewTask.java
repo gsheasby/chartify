9a0925a7f7b45becdd4fc07f576556832c7d06f2
Glenn Sheasby <gsheasby@palantir.com>
2017-09-22 19:34:04 +0100

    Preview: customisable chart size

--- src/main/java/chart/tasks/ChartPreviewTask.java
+++ src/main/java/chart/tasks/ChartPreviewTask.java
@@ -14,12 +14,29 @@ public class ChartPreviewTask {
     public static void main(String[] args) throws IOException {
         int week = Integer.parseInt(args[0]);
 
-        ChartReader reader = new SpotifyChartReader(75);
+        int chartSize = getChartSize(args);
+
+        ChartReader reader = new SpotifyChartReader(chartSize);
         ChartReader derivedReader = new FileChartReader(CsvConstants.DERIVED_FOLDER);
 
         ChartCompiler compiler = new ChartCompiler(reader, derivedReader);
         Chart chart = compiler.compileChart(week);
 
         ChartPrinter.print(chart);
+    }
+
+    private static int getChartSize(String[] args) {
+        int defaultSize = 75;
+        if (args.length > 1) {
+            try {
+                return Integer.parseInt(args[1]);
+            } catch (NumberFormatException ex) {
+                System.err.println(String.format(
+                        "Illegal argument %s; using default chart size %d",
+                        args[1],
+                        defaultSize));
+            }
+        }
+        return defaultSize;
     }
 }
