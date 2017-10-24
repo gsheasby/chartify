6f7996cb37f95d82f351f493de77886127204307
Glenn Sheasby <gsheasby@palantir.com>
2017-10-24 09:34:27 +0100

    Postgres config

--- src/main/java/chart/ChartConfig.java
+++ src/main/java/chart/ChartConfig.java
@@ -6,12 +6,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import chart.postgres.PostgresConfig;
+
 @JsonDeserialize(as = ImmutableChartConfig.class)
 @JsonSerialize(as = ImmutableChartConfig.class)
 @Value.Immutable
 public abstract class ChartConfig {
     @JsonProperty("spotify")
     public abstract SpotifyConfig spotifyConfig();
+
+    @JsonProperty("postgres")
+    public abstract PostgresConfig postgresConfig();
 
     public abstract int chartSize();
     public abstract String csvDestination();
---
+++ src/main/java/chart/postgres/PostgresConfig.java
@@ -0,0 +1,15 @@
+package chart.postgres;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonDeserialize(as = ImmutablePostgresConfig.class)
+@JsonSerialize(as = ImmutablePostgresConfig.class)
+@Value.Immutable
+public abstract class PostgresConfig {
+    public abstract String dbName();
+    public abstract String user();
+    public abstract String password();
+}
