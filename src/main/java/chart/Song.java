fa36af2fad33cc61be558948716b292a08e840a6
Glenn Sheasby <gsheasby@palantir.com>
2017-12-30 17:51:28 +0000

    Song: Case-insensitive comparison
    This ensures that "Bohemian Rhapsody" by "QUEEN" is treated as the same
    song as "bohemian rhapsody" by "queen".

--- src/main/java/chart/Song.java
+++ src/main/java/chart/Song.java
@@ -13,4 +13,27 @@ public abstract class Song {
                 .artist(entry.artist())
                 .build();
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (!(other instanceof Song)) {
+            return false;
+        }
+
+        Song otherSong = (Song) other;
+        return title().equalsIgnoreCase(otherSong.title())
+                && artist().equalsIgnoreCase(otherSong.artist());
+    }
+
+    @Override
+    public int hashCode() {
+        int h = 5381;
+        h += (h << 5) + title().toLowerCase().hashCode();
+        h += (h << 5) + artist().toLowerCase().hashCode();
+        return h;
+    }
 }
