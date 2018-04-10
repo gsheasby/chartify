fd2d1c7e9915180714284a52122d4d959f8d722c
Glenn Sheasby <gsheasby@palantir.com>
2018-04-10 07:26:15 -0700

    Add postgres schema

--- src/main/java/chart/postgres/PostgresConnection.java
+++ src/main/java/chart/postgres/PostgresConnection.java
@@ -2,6 +2,7 @@ package chart.postgres;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,14 +16,70 @@ public class PostgresConnection {
     public void createDatabase() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
 
+        System.out.println("Creating db for user " + config.user());
+
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:postgresql://localhost/",
                 config.user(),
                 config.password())) {
-            Statement statement = conn.createStatement();
-            // TODO guard against sql injection here
-            int output = statement.executeUpdate("CREATE DATABASE " + config.dbName());
-            System.out.println("Create database returned " + 1);
+            createDatabase(conn);
+        }
+
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/" + config.dbName(),
+                config.user(),
+                config.password())) {
+            createSchema(conn);
         }
     }
+
+    private void createDatabase(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(String.format("SELECT 1 FROM pg_database WHERE datname = '%s'", config.dbName()));
+        if (!resultSet.next()) {
+            int output = statement.executeUpdate("CREATE DATABASE " + config.dbName());
+            System.out.println("Create database returned " + output);
+        } else {
+            System.out.println("Skipping creation - DB exists");
+        }
+    }
+
+    private void createSchema(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+
+        statement.executeUpdate("CREATE TABLE artists (" +
+                                        "id varchar(256) CONSTRAINT artistid PRIMARY KEY," +
+                                        "href varchar(512)," +
+                                        "name varchar(256) NOT NULL," +
+                                        "uri varchar(256)" +
+                                        ");");
+
+        statement.executeUpdate("CREATE TABLE tracks (" +
+                                        "id varchar(256) CONSTRAINT trackid PRIMARY KEY," +
+                                        "name varchar(256) NOT NULL," +
+                                        "href varchar(512)," +
+                                        "uri varchar(256)" +
+                                        ");");
+
+        statement.executeUpdate("CREATE TABLE trackArtists (" +
+                                        "track_id varchar(256) REFERENCES tracks," +
+                                        "artist_id varchar(256) REFERENCES artists" +
+                                        ");");
+
+        statement.executeUpdate("CREATE TABLE chart (" +
+                                        "week int CONSTRAINT chartweek PRIMARY KEY," +
+                                        "date date" +
+                                        ");");
+
+        statement.executeUpdate("CREATE TABLE chartEntries (" +
+                                        "chart_week int REFERENCES chart," +
+                                        "position int NOT NULL," +
+                                        "track_id varchar(256) REFERENCES tracks," +
+                                        "CONSTRAINT chart_position PRIMARY KEY(chart_week,position)," +
+                                        "CONSTRAINT pospos CHECK (position > 0)" +
+                                        ");");
+
+    }
+
 }
