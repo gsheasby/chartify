153b46ce200458545bb89aa882b9315b3e52ede1
Glenn Sheasby <gsheasby@palantir.com>
2018-07-19 16:55:40 +0100

    SpotifyApi.getTrack/getArtist

--- src/main/java/chart/spotify/SpotifyApi.java
+++ src/main/java/chart/spotify/SpotifyApi.java
@@ -1,12 +1,18 @@
 package chart.spotify;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.exceptions.WebApiException;
+import com.wrapper.spotify.methods.ArtistSearchRequest;
 import com.wrapper.spotify.methods.PlaylistRequest;
+import com.wrapper.spotify.methods.TrackRequest;
+import com.wrapper.spotify.models.Artist;
 import com.wrapper.spotify.models.ClientCredentials;
 import com.wrapper.spotify.models.Playlist;
+import com.wrapper.spotify.models.Track;
 
 public class SpotifyApi {
     private final Api api;
@@ -20,6 +26,32 @@ public class SpotifyApi {
     public SpotifyApi(Api api, SpotifyConfig config) {
         this.api = api;
         this.config = config;
+    }
+
+    Track getTrack(String trackId) {
+        TrackRequest request = api.getTrack(trackId).build();
+        try {
+            return request.get();
+        } catch (IOException | WebApiException e) {
+            throw new RuntimeException("Couldn't get track", e);
+        }
+    }
+
+    Artist getArtist(String name) throws IOException, WebApiException {
+        ArtistSearchRequest request = api.searchArtists(name).build();
+        List<Artist> items = request.get().getItems();
+        List<Artist> exactMatches = items.stream()
+                                         .filter(artist -> artist.getName().equals(name))
+                                         .collect(Collectors.toList());
+
+        if (exactMatches.isEmpty()) {
+            throw new IllegalStateException("Couldn't find any exact match for artist" + name);
+        }
+        if (exactMatches.size() > 1) {
+            System.out.println("Multiple matches found; returning the first result");
+        }
+
+        return exactMatches.get(0);
     }
 
     Playlist getPlaylist(String playlistId) {
