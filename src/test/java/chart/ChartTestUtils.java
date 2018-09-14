20c197e99a8821789d294779d3878746ced3ad64
Glenn Sheasby <gsheasby@palantir.com>
2018-09-14 09:10:35 +0100

    More deduplication

--- src/test/java/chart/ChartTestUtils.java
+++ src/test/java/chart/ChartTestUtils.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.wrapper.spotify.models.SimpleArtist;
 import com.wrapper.spotify.models.Track;
 
+import chart.spotify.ChartPosition;
 import chart.spotify.SpotifyChartEntry;
 
 public class ChartTestUtils {
@@ -22,5 +23,22 @@ public class ChartTestUtils {
         track.setName("title");
         track.setArtists(ImmutableList.of(artist));
         return track;
+    }
+
+    static SpotifyChartEntry threeWeeks() {
+        Iterable<ChartPosition> run = ImmutableList.of(
+                pos(1, 10), pos(2, 7), pos(3, 5)
+        );
+        return SpotifyChartEntry.builder()
+                                .track(ChartTestUtils.track())
+                                .position(5)
+                                .lastPosition(7)
+                                .weeksOnChart(3)
+                                .chartRun(run)
+                                .build();
+    }
+
+    private static ChartPosition pos(int week, int position) {
+        return ChartPosition.builder().week(week).position(position).build();
     }
 }
--- src/test/java/chart/PlainTextChartFormatterTest.java
+++ src/test/java/chart/PlainTextChartFormatterTest.java
@@ -6,21 +6,15 @@ import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableList;
-import com.wrapper.spotify.models.Track;
-
-import chart.spotify.ChartPosition;
 import chart.spotify.SpotifyChart;
 import chart.spotify.SpotifyChartEntry;
 
 public class PlainTextChartFormatterTest {
     private ChartFormatter formatter;
-    private Track track;
 
     @Before
     public void setUp() {
         formatter = new PlainTextChartFormatter();
-        track = ChartTestUtils.track();
     }
 
     @Test
@@ -47,7 +41,7 @@ public class PlainTextChartFormatterTest {
 
     @Test
     public void lastPositionFormat() {
-        SpotifyChartEntry entry = threeWeeks();
+        SpotifyChartEntry entry = ChartTestUtils.threeWeeks();
 
         String expected = "05 (07) 3 artist - title [10, 7, 5]";
         String line = formatter.getLine(entry);
@@ -56,28 +50,10 @@ public class PlainTextChartFormatterTest {
 
     @Test
     public void dropoutFormat() {
-        SpotifyChartEntry entry = threeWeeks();
+        SpotifyChartEntry entry = ChartTestUtils.threeWeeks();
 
         String expected = "-- (05) 3 artist - title [10, 7, 5]";
         String line = formatter.getDropoutText(entry);
         assertEquals(expected, line);
     }
-
-    private SpotifyChartEntry threeWeeks() {
-        Iterable<ChartPosition> run = ImmutableList.of(
-                pos(1, 10), pos(2, 7), pos(3, 5)
-        );
-        return SpotifyChartEntry.builder()
-                                .track(track)
-                                .position(5)
-                                .lastPosition(7)
-                                .weeksOnChart(3)
-                                .chartRun(run)
-                                .build();
-    }
-
-    private ChartPosition pos(int week, int position) {
-        return ChartPosition.builder().week(week).position(position).build();
-    }
-
 }
