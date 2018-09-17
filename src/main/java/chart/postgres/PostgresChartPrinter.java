8428eeca6cbda1041e8385478bfd94fb86555ea9
Glenn Sheasby <gsheasby@palantir.com>
2018-09-17 08:26:37 +0100

    Printing bubblers

--- src/main/java/chart/postgres/PostgresChartPrinter.java
+++ src/main/java/chart/postgres/PostgresChartPrinter.java
@@ -1,11 +1,16 @@
 package chart.postgres;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import chart.ChartFormatter;
 import chart.ChartPrinter;
 import chart.spotify.SpotifyChart;
 import chart.spotify.SpotifyChartEntry;
 
 public class PostgresChartPrinter implements ChartPrinter<SpotifyChart> {
+    private static final int CUTOFF = 60;
+
     private final ChartFormatter formatter;
 
     public PostgresChartPrinter(ChartFormatter formatter) {
@@ -17,14 +22,29 @@ public class PostgresChartPrinter implements ChartPrinter<SpotifyChart> {
         printChartHeader(chart);
         System.out.println();
 
+        List<SpotifyChartEntry> dropouts = new ArrayList<>(chart.dropouts());
         for (SpotifyChartEntry entry : chart.entries()) {
-            printEntry(entry);
+            if (entry.position() <= CUTOFF) {
+                printEntry(entry);
+            } else if (wasInLastWeek(entry)) {
+                dropouts.add(entry);
+            } else if (isBubbler(entry)) {
+                printBubbler(entry);
+            }
         }
         System.out.println();
 
-        for (SpotifyChartEntry dropout : chart.dropouts()) {
+        for (SpotifyChartEntry dropout : dropouts) {
             printDropout(dropout);
         }
+    }
+
+    private boolean wasInLastWeek(SpotifyChartEntry entry) {
+        return entry.lastPosition().isPresent() && entry.lastPosition().get() <= CUTOFF;
+    }
+
+    private boolean isBubbler(SpotifyChartEntry entry) {
+        return entry.chartRun().stream().noneMatch(pos -> pos.position() <= CUTOFF);
     }
 
     private void printChartHeader(SpotifyChart chart) {
@@ -33,6 +53,10 @@ public class PostgresChartPrinter implements ChartPrinter<SpotifyChart> {
 
     private void printEntry(SpotifyChartEntry entry) {
         System.out.println(formatter.getLine(entry));
+    }
+
+    private void printBubbler(SpotifyChartEntry entry) {
+        System.out.println(formatter.getBubbler(entry));
     }
 
     private void printDropout(SpotifyChartEntry entry) {
