d25572a8ebd7ca9348bea65a994d3bf7e3f3cf85
Glenn Sheasby <gsheasby@palantir.com>
2018-12-14 09:29:50 +0000

    Add playlists config field

--- src/main/java/chart/spotify/SpotifyConfig.java
+++ src/main/java/chart/spotify/SpotifyConfig.java
@@ -1,6 +1,7 @@
 package chart.spotify;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.immutables.value.Value;
 
@@ -15,7 +16,14 @@ public abstract class SpotifyConfig {
     public abstract String clientId();
     public abstract String clientSecret();
     public abstract String userName();
+
+    /**
+     * @deprecated use playlists instead
+     */
+    @Deprecated
     public abstract String playlistId();
+
+    public abstract Map<String, String> playlists();
 
     @Value.Default
     // Using a HashMap explicitly to work around https://github.com/immutables/immutables/issues/680
