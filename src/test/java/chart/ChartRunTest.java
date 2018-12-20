fbe98dc31adc5a25e679851225915135879b14d8
Glenn Sheasby <gsheasby@palantir.com>
2018-12-20 14:56:26 +0000

    More tests for ChartRun

--- src/test/java/chart/ChartRunTest.java
+++ src/test/java/chart/ChartRunTest.java
@@ -3,8 +3,12 @@ package chart;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class ChartRunTest {
     private static final Integer ENTRY_WEEK = 1;
@@ -38,29 +42,50 @@ public class ChartRunTest {
 
     @Test
     public void higher_scores_are_better() {
-        ChartRun betterRun = newChartRun();
-        betterRun.add(2);
-        betterRun.add(3);
-
-        ChartRun worseRun = newChartRun();
-        worseRun.add(2);
-        worseRun.add(42);
-
-        assertEquals(-1L, betterRun.compareTo(worseRun));
+        firstRunShouldBeBetter(
+                list(2, 3),
+                list(2, 42));
     }
 
     @Test
     public void higher_scores_are_better_with_fewer_weeks_and_worse_peak() {
-        ChartRun betterRun = newChartRun();
-        betterRun.add(2);
-        betterRun.add(3);
+        firstRunShouldBeBetter(
+                list(2, 3),
+                list(1, 42, 42));
+    }
 
-        ChartRun worseRun = newChartRun();
-        worseRun.add(1);
-        worseRun.add(42);
-        worseRun.add(42);
+    @Test
+    public void more_weeks_are_better() {
+        firstRunShouldBeBetter(
+                list(58, 60, 60),
+                list( 58, 59)
+        );
+    }
 
-        assertEquals(-1L, betterRun.compareTo(worseRun));
+    @Test
+    public void higher_peak_is_better() {
+        firstRunShouldBeBetter(
+                list(1, 4),
+                list(2, 3)
+        );
+    }
+
+    @Test
+    public void higher_peak_is_better_even_with_fewer_weeks() {
+        firstRunShouldBeBetter(
+                list(59),
+                list(60, 60)
+        );
+    }
+
+    @Test
+    public void weeks_at_peak_do_not_matter() {
+        bothRunsShouldBeEqual(list(1, 1, 3), list(1, 2, 2));
+    }
+
+    @Test
+    public void lower_positions_in_run_do_not_matter() {
+        bothRunsShouldBeEqual(list(1, 3, 4), list(1, 2, 5));
     }
 
     private ChartRun newChartRun() {
@@ -69,5 +94,29 @@ public class ChartRunTest {
 
     private void runShouldHaveScore(ChartRun run, Integer expected) {
         assertEquals(expected, run.getScore());
+    }
+
+    private void firstRunShouldBeBetter(List<Integer> betterPositions, List<Integer> worsePositions) {
+        ChartRun betterRun = newChartRun();
+        betterPositions.forEach(betterRun::add);
+
+        ChartRun worseRun = newChartRun();
+        worsePositions.forEach(worseRun::add);
+
+        assertEquals(-1L, betterRun.compareTo(worseRun));
+    }
+
+    private void bothRunsShouldBeEqual(List<Integer> firstRunPositions, List<Integer> secondRunPositions) {
+        ChartRun firstRun = newChartRun();
+        firstRunPositions.forEach(firstRun::add);
+
+        ChartRun secondRun = newChartRun();
+        secondRunPositions.forEach(secondRun::add);
+
+        assertEquals(0L, firstRun.compareTo(secondRun));
+    }
+
+    private List<Integer> list(Integer... positions) {
+        return Lists.newArrayList(positions);
     }
 }
