049cc3836928133c99a93936038bae668358eb7f
Glenn Sheasby <gsheasby@palantir.com>
2019-01-14 08:06:49 +0000

    Better YEC printing (#19)
    * Add limits to YEC tasks
    
    * Change YEC preview print format

--- src/main/java/chart/postgres/YearEndChartPrinter.java
+++ src/main/java/chart/postgres/YearEndChartPrinter.java
@@ -4,18 +4,26 @@ import java.util.Iterator;
 import java.util.List;
 
 import chart.ChartRun;
+import chart.Song;
 
 public class YearEndChartPrinter {
-    public static void printYearEndChart(List<ChartRun> yec) {
+    public static void printYearEndChart(List<ChartRun> yec, int limit) {
         Iterator<ChartRun> yecIterator = yec.iterator();
 
-        for (int pos = 1; yecIterator.hasNext(); pos++) {
+        for (int pos = 1; yecIterator.hasNext() && pos <= limit; pos++) {
             ChartRun chartRun = yecIterator.next();
             printSingleSong(pos, chartRun);
         }
     }
 
     public static void printSingleSong(int pos, ChartRun chartRun) {
-        System.out.println(String.format("%02d\t%s", pos, chartRun.toString()));
+//        System.out.println(String.format("%02d\t%s", pos, chartRun.toString()));
+        Song song = chartRun.getSong();
+        System.out.println(String.format("%02d\t%s\t-\t%s\t(#%d, %s)",
+                                         pos,
+                                         song.title(),
+                                         song.artist(),
+                                         chartRun.getPeak(),
+                                         chartRun.getEntryDate().toString("MMMM")));
     }
 }
