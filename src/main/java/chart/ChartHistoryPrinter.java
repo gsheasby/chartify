f49de9d55ccf88b912adb29661c90978604f2c86
Glenn Sheasby <gsheasby@palantir.com>
2019-05-24 08:31:11 +0100

    Pull out method around api call

--- src/main/java/chart/ChartHistoryPrinter.java
+++ src/main/java/chart/ChartHistoryPrinter.java
@@ -7,6 +7,7 @@ import com.wrapper.spotify.models.Artist;
 import com.wrapper.spotify.models.SimpleArtist;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ChartHistoryPrinter {
     private final SpotifyApi api;
@@ -18,18 +19,26 @@ public class ChartHistoryPrinter {
     }
 
     public void printHistory(String artist) throws IOException {
-        try {
-            Artist spotifyArtist = api.getArtist(artist);
+        Optional<Artist> maybeSpotifyArtist = getArtist(artist);
 
-            SimpleArtist simpleArtist = new SimpleArtist();
+        // TODO isPresent check
+        Artist spotifyArtist = maybeSpotifyArtist.get();
+
+        SimpleArtist simpleArtist = new SimpleArtist();
             simpleArtist.setId(spotifyArtist.getId());
             simpleArtist.setName(spotifyArtist.getName());
             simpleArtist.setHref(spotifyArtist.getHref());
             simpleArtist.setUri(spotifyArtist.getUri());
 
             printer.printHistory(simpleArtist);
+    }
+
+    private Optional<Artist> getArtist(String artist) throws IOException {
+        try {
+            return Optional.of(api.getArtist(artist));
         } catch (WebApiException e) {
-            e.printStackTrace();
+            System.out.println("Couldn't load artist from Spotify!");
+            return Optional.empty();
         }
     }
 }
